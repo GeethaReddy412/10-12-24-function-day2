10b9f8c04d735ae9f5ef1c2b6e2c7811c0a5af24
GeethaReddy412 <geethareddy8008503@gmail.com>
2024-12-11 12:28:31 +0530

    function

---
+++ task1.js
@@ -0,0 +1,11 @@
+
+// 7. Write a function that takes a number as a parameter and returns true if the number is a even number, otherwise false.
+function evenNumbers(a,b){
+  if (a%2==0){
+    console.log("it is a even number");
+  }
+  else{
+    console.log("it is not a even number");
+  }
+}
+evenNumbers(2,4);
